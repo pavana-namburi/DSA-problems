cb911e3bf700ade9804db314323ced3a5cb79e8a
pavana-namburi <pavananamburi1@gmail.com>
2025-09-12 21:40:22 +0530

    find isogram or not

--- findunique.java
+++ findunique.java
@@ -4,16 +4,17 @@ public class findunique {
     public static void main(String arg[]){
         Scanner sc=new Scanner(System.in);
         String s=sc.nextLine();
+        s.toLowerCase();
         HashSet<Character> hs=new HashSet<>();
         for(char i:s.toCharArray()){
-            hs.add(i);
+            if(!hs.contains(i))
+                hs.add(i);
+            else{
+                System.out.println("not isogram");
+                System.exit(0);
+            }
         }
-        if(hs.size()==26){
-            System.out.println("panagram");
-        }
-        else{
-            System.out.println(" not panagram");
-        }
+        System.out.println("isogram");
         sc.close();
     }
 }
