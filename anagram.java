41c6f4b5bb9f647bf0d90d3b8a544aacfa4d1bf6
pavana-namburi <pavananamburi1@gmail.com>
2025-09-12 20:50:41 +0530

    method-2

--- anagram.java
+++ anagram.java
@@ -8,7 +8,7 @@ public class anagram {
         s2.toLowerCase();
         char a[]=s1.toCharArray();
         char b[]=s2.toCharArray();
-        Arrays.sort(a);
+        /*Arrays.sort(a);
         Arrays.sort(b);
         for(int i=0;i<s1.length();i++){
             if(a[i]!=b[i]){
@@ -17,7 +17,18 @@ public class anagram {
             }
 
         }
-        System.out.println("anagram");
+        System.out.println("anagram");*/
+        HashMap<Character,Integer> hm1=new HashMap<>();
+        HashMap<Character,Integer> hm2=new HashMap<>();
+        for(int i=0;i<s1.length();i++){
+            hm1.put(a[i],hm1.getOrDefault(a[i],0)+1); 
+            hm2.put(b[i],hm2.getOrDefault(b[i],0)+1); 
+        }
+        if(hm1.equals(hm2)){
+            System.out.println("anagram");
+            System.exit(0);
+        }
+        System.out.println("not anagram");
         sc.close();
     }
 }
