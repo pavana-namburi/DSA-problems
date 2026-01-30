598925f53ebaceb02dab327e50562f902a64518a
pavana-namburi <pavananamburi1@gmail.com>
2026-01-30 20:13:34 +0530

    coded application

---
+++ Ecom.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Ecom {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your choice");
+        int n=sc.nextInt();
+        sc.close();
+    }
+}
